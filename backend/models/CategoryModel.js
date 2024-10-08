60e7e9ee69098c023ad3e47eff208c5b18de6c74
Kuba <kubaa0105@gmail.com>
2024-10-08 21:40:49 +0200

    addcategory naprawic

--- backend/models/CategoryModel.js
+++ backend/models/CategoryModel.js
@@ -4,7 +4,7 @@ const Joi = require("joi");
 const categorySchema = new mongoose.Schema({
     nazwaKategorii: { type: String, required: true },
     dziedzinaNaukowa: { type: String, enum: ['Matematyka', 'Przyroda', 'Zadania logiczne', 'Historia'] ,required: true },
-    przedzialWiekowy: { type: String, enum: ['3','4','5','6','7','8','9'], required: true },
+    przedzialWiekowy: { type: String, enum: ['3','4','5'], required: true },
     zadania: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
 })
 
@@ -13,11 +13,13 @@ const Category = mongoose.model("Category", categorySchema)
 const validate = (data) => {
     const schema = Joi.object({
         nazwaKategorii: Joi.string().required().label("Nazwa kategorii"),
+        dziedzinaNaukowa: Joi.string().valid('Matematyka', 'Przyroda', 'Zadania logiczne', 'Historia').required(),
+        przedzialWiekowy: Joi.string().valid('3', '4', '5').required(),
     })
 
     return schema.validate(data)
 }
 
 
-module.exports = {Category, validate }
+module.exports = { Category, validate }
 
--- frontend/src/components/AddCategory/index.jsx
+++ frontend/src/components/AddCategory/index.jsx
@@ -1,77 +1,147 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import {useState} from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./styles.module.css";
 
-const AddCategory= () => {
-    const [nazwa, setNazwa] = useState('');
-    const [dziedzinaNaukowa, setDziedzinaNaukowa] = useState('');
-    const [przedzialWiekowy, setPrzedzialWiekowy] = useState('');
+
+const AddTasks = () => {
+    const [data, setData] = useState({
+        nazwaKategorii: "",
+        dziedzinaNaukowa: "",
+        przedzialWiekowy: "",
+    })
+
+    const [error, setError] = useState("")
+    const navigate = useNavigate()
+
+    const handleChange = ({ currentTarget: input }) => {
+        setData({...data, [input.name]: input.value})
+    }
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
+        const token = localStorage.getItem("token");
+        e.preventDefault()
 
-        const newCategory = {
-            nazwa,
-            dziedzinaNaukowa,
-            przedzialWiekowy
-        };
+        if(token){
+            try{
+                const url = "http://localhost:8080/api/categories"
+                const headers = {
+                    "x-access-token": token,
+                };
 
-        try {
-            const response = await axios.post('http://localhost:8080/api/categories', newCategory);
-            console.log('Kategoria została dodana', response.data);
-            // Ewentualnie zresetuj formularz
-            setNazwa('');
-            setDziedzinaNaukowa('');
-            setPrzedzialWiekowy('');
-        } catch (error) {
-            console.error('Błąd podczas dodawania kategorii:', error);
+                const { data:res } = await axios.post(url,data, { headers })
+                navigate("/")
+                console.log(res.message)
+            } catch (error){
+                if(error.response && error.response.status >= 400 && error.response.status <= 500) {
+                    setError(error.response.data.message)
+                    //localStorage.removeItem("token")
+                    //window.location.reload()
+                }
+            }
         }
-    };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Nazwa kategorii:</label>
-                <input
-                    type="text"
-                    value={nazwa}
-                    onChange={(e) => setNazwa(e.target.value)}
-                    required
-                />
+    }
+
+    const handleLogout = () => {
+        localStorage.removeItem("token")
+        window.location.reload()
+    }
+
+    const [pokazMenu, ustawPokazMenu] = useState(false);
+
+    const showMenu = () => {
+        ustawPokazMenu(!pokazMenu);
+    }
+
+    return (<div className={styles.main_container}>
+            <nav className={styles.navbar}>
+                <div className={styles.nav_left}>
+                    <button className={styles.white_btn} onClick={showMenu}> Menu</button>
+                </div>
+                <div className={styles.nav_center}>
+                    <Link to="/">
+                        <a className={styles.text_logo}> TeachChild</a>
+                    </Link>
+                </div>
+                <div className={styles.nav_right}>
+                    <Link to="/contact">
+                        <button className={styles.white_btn}> Kontakt</button>
+                    </Link>
+                    <button className={styles.white_btn} onClick={handleLogout}>
+                        Wyloguj się
+                    </button>
+                </div>
+
+
+            </nav>
+            {pokazMenu && (
+                <div className={styles.menu}>
+                    <ul>
+                        <li>
+                            3 Lata:
+                        </li>
+                        <li>
+                            4 Lata:
+                        </li>
+                        <li>
+                            5 Lat:
+                        </li>
+                        <li>
+                            6 lat:
+                        </li>
+                        <li>
+                            7 lat:
+                        </li>
+                    </ul>
+                </div>
+            )}
+            <div className={styles.signup_container}>
+                <div className={styles.signup_form_container}>
+                    <div className={styles.right}>
+                        <form className={styles.form_container} onSubmit={handleSubmit}>
+                            <h1>Dodaj kategorię</h1>
+                            <input
+                                type="text"
+                                placeholder="Nazwa kategorii"
+                                name="nazwaKategorii"
+                                onChange={handleChange}
+                                value={data.nazwaKategorii}
+                                required
+                                className={styles.input}
+                            />
+                            <select name="dziedzinaNaukowa" onChange={handleChange} value={data.dziedzinaNaukowa}
+                                    required>
+                                <option value="" disabled>
+                                    Wybierz dziedzinę naukową
+                                </option>
+                                <option value="Matematyka">Matematyka</option>
+                                <option value="Przyroda">Przyroda</option>
+                                <option value="Zadania logiczne">Zadania logiczne</option>
+                                <option value="Historia">Historia</option>
+                            </select>
+                            <input
+                                type="text"
+                                placeholder="Przedział wiekowy"
+                                name="przedzialWiekowy"
+                                onChange={handleChange}
+                                value={data.przedzialWiekowy}
+                                required
+                                className={styles.input}
+                            />
+                            {error && <div className={styles.error_msg}>{error}</div>}
+                            <button type="submit" className={styles.green_btn}>
+                                Wyślij zgłoszenie
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div>
-                <label>Dziedzina naukowa:</label>
-                <select
-                    value={dziedzinaNaukowa}
-                    onChange={(e) => setDziedzinaNaukowa(e.target.value)}
-                    required
-                >
-                    <option value="">Wybierz dziedzinę</option>
-                    <option value="Matematyka">Matematyka</option>
-                    <option value="Przyroda">Przyroda</option>
-                    <option value="Zadania logiczne">Zadania logiczne</option>
-                    <option value="Historia">Historia</option>
-                </select>
-            </div>
-            <div>
-                <label>Przedział wiekowy:</label>
-                <select
-                    value={przedzialWiekowy}
-                    onChange={(e) => setPrzedzialWiekowy(e.target.value)}
-                    required
-                >
-                    <option value="">Wybierz przedział wiekowy</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                </select>
-            </div>
-            <button type="submit" onClick={handleSubmit}>Dodaj kategorię</button>
-        </form>
+            <footer className={styles.footer}>
+                <Link to="/contact">Kontakt</Link> <br/>
+                &copy; 2024 TeachChild. Wszelkie prawa zastrzeżone.
+            </footer>
+        </div>
     );
 };
-
-export default AddCategory
+export default AddTasks
